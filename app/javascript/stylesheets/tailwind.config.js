8dfc654c57bc4cbe7419214408cf43b5c2fc363c
r0xk0n <konradrycerz@gmail.com>
2021-01-06 23:58:46 +0100

    rails init

---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
+
