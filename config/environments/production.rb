a686c2337aa442c40ef483021c81eb51ef8165d5
r0xk0n <konradrycerz@gmail.com>
2021-01-26 23:55:32 +0100

    [fix] heroku default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cannabishouse_production"
 
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN"] }
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
