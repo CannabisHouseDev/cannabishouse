aafc5984d5b78b1a7ed1addddd49052475f0eadc
r0xk0n <konradrycerz@gmail.com>
2021-02-04 00:06:26 +0100

    [fix] another try

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get 'under_construction', to: 'home#under_construction'
-  get 'blog', to: 'home#index'
+  get 'blog', to: 'posts#index'
   get 'map', to: 'home#map', as: :map
   get ':id', to: 'pages#show', as: :page
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
