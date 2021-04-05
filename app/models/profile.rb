982aca96a284660ff2cdfbfb67196fcf39dd6d5a
r0xk0n <konradrycerz@gmail.com>
2021-04-05 22:24:32 +0200

    [feat] add profile factory and profile model spec

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,6 @@ class Profile < ApplicationRecord
   validates :pesel, format: { with: /\A(\d{11})\z/, message: I18n.t('activerecord.attributes.profile.pesel_11_digit')}
 
   enum role: [:user, :admin, :curator, :coordinator, :consultant, :editor, :researcher, :doctor]
+
+  enum gender: [:male, :female]
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,16 @@ FactoryBot.define do
     password  { "1234123412341234" }
     password_confirmation { "1234123412341234" }
   end
+  factory :profile do
+    role { 0 }
+    first_name { "John" }
+    last_name { "Hammond" }
+    nick_name { "JK" }
+    pesel { "88111132145" }
+    gender { 0 }
+    skills  { "chess player, painter, roller" }
+    illness { "allergy" }
+    contact_number { "555565777" }
+    avatar { "dlugiurldoobrazka" }
+  end
 end
