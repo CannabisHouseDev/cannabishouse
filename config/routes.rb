7a55b2bfc2fcfbbcf52e0a8449bd4a202301b674
God <shalashtein@gmail.com>
2021-05-15 16:11:08 +0300

    Scoped routes under locale

--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,28 @@ Rails.application.routes.draw do
 
     root to: "users#index"
   end
-  
-  resources :posts
-  
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions:      'users/sessions'
-  }
-  
-  resources :users, only: [:show, :index, :edit, :update, :destroy] do
-    resources :addresses
-    resources :profiles
+
+    devise_for :users, controllers: {
+      registrations: 'users/registrations',
+      sessions:      'users/sessions'
+    }
+
+  scope '(:locale)', locale: /en/ do
+    root 'pages/landing'
+
+    resources :posts
+    resource :user, only: [:update]
+    resources :users, only: [:show, :index, :edit, :update, :destroy] do
+      resources :addresses
+      resources :profiles
+    end
+
+    get 'home/index'
+    get 'under_construction', to: 'home#under_construction'
+    get 'blog', to: 'posts#index'
+    get 'map', to: 'home#map', as: :map
+    get ':id', to: 'pages#show', as: :page
   end
-  resource :user, only: [:update]
-  
-  get 'home/index'
-  root 'home#index'
-  get 'under_construction', to: 'home#under_construction'
-  get 'blog', to: 'posts#index'
-  get 'map', to: 'home#map', as: :map
-  get ':id', to: 'pages#show', as: :page
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
