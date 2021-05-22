63caa0e08c1bfe9a7ad8458aa731a4d54945f489
God <shalashtein@gmail.com>
2021-05-22 17:28:28 +0300

    added an exception for user authentication on landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: 'landing'
 
   def landing; end
 
