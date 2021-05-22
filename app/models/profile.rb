2f8e48a9462a567cd172658410e95539124015a3
God <shalashtein@gmail.com>
2021-05-22 19:20:51 +0300

    added validation on onboarding form update

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,11 +5,21 @@ class Profile < ApplicationRecord
 
   has_one_attached :avatar
 
-  validates :pesel, format: { with: /\A(\d{11})\z/, message: I18n.t('activerecord.attributes.profile.pesel_11_digit') },
+  validates :pesel, format: { with: /\A(\d{11})\z/ },
                     if: :participant?
 
   enum role: %i[user participant admin dispensary researcher doctor warehouse],
        _default: 0
 
   enum gender: %i[male female]
+
+  validates :first_name, presence: true, on: :update
+  validates :last_name, presence: true, on: :update
+  validates :gender, presence: true, on: :update
+  validates :pesel, presence: true, on: :update
+  validates :contact_number, presence: true, on: :update
+
+  def checkInfo
+
+  end
 end
