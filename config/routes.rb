fa252221e0915af5e266bb2b730a3c8b63c68950
shalashtein <shalashtein@gmail.com>
2021-06-03 16:43:45 +0300

    routing bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -18,16 +18,25 @@ Rails.application.routes.draw do
   scope '(:locale)', locale: /en/ do
     authenticated :user do
       root 'portals#role_router', as: :authenticated_root
+
+      get 'welcome', to: 'pages#onboarding', as: 'onboarding'
+
+      get 'participant', to: 'participant_portal#index', as: 'participant_portal'
+      get 'dispensary', to: 'dispensary_portal#index', as: 'dispensary_portal'
+      get 'doctor', to: 'doctor_portal#index', as: 'doctor_portal'
+      get 'researcher', to: 'researcher_portal#index', as: 'researcher_portal'
+
+      get 'map', to: 'home#map', as: :map
+      get 'billing', to: 'dispensary_portal#billing', as: 'billing'
+      get 'stock', to: 'dispensary_portal#stock', as: 'stock'
+      get 'search', to: 'dispensary_portal#search', as: 'user_lookup'
+      get 'trasnfer', to: 'dispensary_portal#transfer', as: 'dispensary_to_participant'
     end
 
     root 'pages#landing'
 
-    get 'welcome', to: 'pages#onboarding', as: 'onboarding'
-
-    get 'participant', to: 'participant_portal#index', as: 'participant_portal'
-    get 'dispensary', to: 'dispensary_portal#index', as: 'dispensary_portal'
-    get 'doctor', to: 'doctor_portal#index', as: 'doctor_portal'
-    get 'researcher', to: 'researcher_portal#index', as: 'researcher_portal'
+    get 'about', to: 'pages#about', as: 'about'
+    get 'media', to: 'pages#media', as: 'media'
 
     resources :posts
     resource :user, only: [:update]
@@ -35,17 +44,6 @@ Rails.application.routes.draw do
       resources :addresses
       resources :profiles
     end
-
-    get 'home/index'
-    get 'under_construction', to: 'home#under_construction'
-    get 'blog', to: 'posts#index'
-    get 'map', to: 'home#map', as: :map
-    get 'about', to: 'pages#about', as: 'about'
-    get 'media', to: 'pages#media', as: 'media'
-    get 'billing', to: 'dispensary_portal#billing', as: 'billing'
-    get 'stock', to: 'dispensary_portal#stock', as: 'stock'
-    get 'search', to: 'dispensary_portal#search', as: 'user_lookup'
-    get 'trasnfer', to: 'dispensary_portal#transfer', as: 'dispensary_to_participant'
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
