2a43a25f4258e6fe56fc29e372052ecd34bef124
shalashtein <shalashtein@gmail.com>
2021-06-11 11:45:15 +0300

    Machine State skeleton

--- app/models/material.rb
+++ app/models/material.rb
@@ -29,6 +29,7 @@
 #  fk_rails_...  (owner_id => users.id)
 #
 class Material < ApplicationRecord
+  inlcude AASM
   has_paper_trail
   belongs_to :material_type, class_name: "MaterialType", foreign_key: 'material_type_id'
   belongs_to :owner, class_name: "User", foreign_key: 'owner_id', dependent: :destroy
@@ -47,5 +48,49 @@ class Material < ApplicationRecord
                     receiver: receiver,
                     weight: amount)
     transfer
+  end
+
+  aasm do
+    state :ready, initial: true
+          :validating_dispensary
+          :validating_stock
+          :validating_profile
+          :validating_evaluation
+          :validating_amount
+          :validating_credits
+          :validating_duplicate
+
+    event :process do
+      transitions from: :ready, to: :validating_dispensary
+    end
+
+    event :validate_dispensary do
+      transitions from: :validating_dispensary, to: :validating_stock
+    end
+
+    event :validate_stock do
+      transitions from: :validating_stock, to: :validating_profile
+    end
+
+    event :validate_profile do
+      transitions from: :validating_profile, to: :validating_evaluation
+    end
+
+    event :validate_evaluation do
+      transitions from: :validating_evaluation, to: :validating_amount
+    end
+
+    event :validate_amount do
+      transitions from: :validating_amount, to: :validating_credits
+    end
+
+    event :validate_credits do
+      transitions from: :validating_credits, to: :validating_duplicate
+    end
+
+    event :finalize do
+      transitions from: :validating_duplicate, to: :ready
+    end
+
   end
 end
