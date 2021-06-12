06ed3368906884bbfd92237262961801f3d37bd0
shalashtein <shalashtein@gmail.com>
2021-06-12 16:04:14 +0300

    removed redundant block

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -27,8 +27,8 @@
 #
 class Transfer < ApplicationRecord
   has_paper_trail
-  belongs_to :sender_material, class_name: "Material", foreign_key: 'sender_material_id', dependent: :destroy
-  belongs_to :receiver_material, class_name: "Material", foreign_key: 'receiver_material_id', dependent: :destroy
-  belongs_to :sender, class_name: "User", foreign_key: 'sender_id', dependent: :destroy
-  belongs_to :receiver, class_name: "User", foreign_key: 'receiver_id', dependent: :destroy
+  belongs_to :sender_material, class_name: "Material", foreign_key: 'sender_material_id'
+  belongs_to :receiver_material, class_name: "Material", foreign_key: 'receiver_material_id'
+  belongs_to :sender, class_name: "User", foreign_key: 'sender_id'
+  belongs_to :receiver, class_name: "User", foreign_key: 'receiver_id'
 end
