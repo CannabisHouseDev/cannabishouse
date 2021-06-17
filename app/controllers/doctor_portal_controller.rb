41becadea5eb9ac2ba3cd39bfc66cacc98837ba0
shalashtein <shalashtein@gmail.com>
2021-06-17 13:40:33 +0300

    cleaning up

--- app/controllers/doctor_portal_controller.rb
+++ app/controllers/doctor_portal_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class DoctorPortalController < ApplicationController
-  def index
-    @start_time = 8
-    @end_time = 20
-  end
+  def index; end
 
   def appointments; end
 
-  def calendar; end
+  def calendar
+    @start_time = 8
+    @end_time = 20
+  end
 end
