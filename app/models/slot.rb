671080987ac94b360abad92cb57bf4e92e4d6c5e
shalashtein <shalashtein@gmail.com>
2021-06-21 23:08:58 +0300

    minor fixes

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -2,7 +2,8 @@ class Slot < ApplicationRecord
   belongs_to :doctor, class_name: 'User'
   has_many :appointments
 
-  VALID_TIME_FORMAT= /(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/
+  VALID_TIME_FORMAT= /^\d{1,2}:\d{1,2}$/
   validates :time, format:{with:VALID_TIME_FORMAT}
+  validates :day, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 6 }
 
 end
--- db/migrate/20210621103617_create_slots.rb
+++ db/migrate/20210621103617_create_slots.rb
@@ -2,6 +2,7 @@ class CreateSlots < ActiveRecord::Migration[6.1]
   def change
     create_table :slots do |t|
       t.boolean :booked, default: false
+      t.integer :day, default: 1
       t.string :time
 
       t.timestamps
