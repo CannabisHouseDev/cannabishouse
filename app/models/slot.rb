d5c2ebf11736c7a1b419cdf49a515c8808ffd143
r0xk0n <konradrycerz@gmail.com>
2021-06-25 19:00:04 +0200

    fix regex

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -12,7 +12,7 @@
 class Slot < ApplicationRecord
   belongs_to :doctor, class_name: 'User'
 
-  VALID_TIME_FORMAT= /^\d{1,2}:\d{1,2}$/
+  VALID_TIME_FORMAT=  /\A([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]\z/
   validates :time, format: { with: VALID_TIME_FORMAT }
   validates :day, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 6 }
 
