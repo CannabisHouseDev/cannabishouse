4309afec7143d7c9a60bf01bdb02afce47ae25ca
emeren <tomasz.szenborn@gmail.com>
2021-07-04 16:59:52 +0200

    landing page styles

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,18 +1,35 @@
 // tailwind.config.js
+const { colors: defaultColors, colors } = require('tailwindcss/defaultTheme');
+
+// tailwind.config.js
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      primary: {
+        50: '#F7FEE7',
+        100: '#ECFCCB',
+        200: '#D9F99D',
+        300: '#BEF264',
+        400: '#A3E635',
+        500: '#84CC16',
+        600: '#65A30D',
+        DEFAULT: '#65A30D',
+        700: '#4D7C0F',
+        800: '#3F6212',
+        900: '#365314',
+      },
+    },
   },
   variants: {
-    backgroundColor: ['responsive', 'hover', 'focus','active'],
-    justifyContent: ['hover']
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    justifyContent: ['hover'],
+    translate: ['responsive', 'hover', 'group-hover', 'focus'],
+    transform: ['group-hover'],
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
 
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
+};
