2cf2ef4e788af9d1bd050981db05b04fba767083
emeren <tomasz.szenborn@gmail.com>
2021-07-06 13:42:52 +0200

    add mobile navigation

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
     extend: {},
     colors: {
       ...colors,
@@ -29,6 +37,7 @@ module.exports = {
     justifyContent: ['hover'],
     translate: ['responsive', 'hover', 'group-hover', 'focus'],
     transform: ['group-hover'],
+    display: ['responsive'],
   },
 
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
