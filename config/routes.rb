24be6803a4293e5c62bcc41d8b103358f6b43fde
Marcin Wolski <wolski.dev@gmail.com>
2021-07-13 22:49:23 +0200

    fix: Use devise sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    registrations: 'users/registrations'
   }
 
   scope '(:locale)', locale: /en/ do
